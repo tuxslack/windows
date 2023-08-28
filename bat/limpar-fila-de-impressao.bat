86747e451f79272da1009477d4d9f4cb744e72b1
tuxslack <33039864+tuxslack@users.noreply.github.com>
2023-08-28 13:27:25 -0300

    Add files via upload

---
+++ bat/limpar-fila-de-impressao.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Interrompendo o servico de impressao...
+echo.
+
+net stop spooler
+
+echo Deletando os trabalhos na fila de impressao...
+echo.
+
+del /Q /F /S %systemroot%\System32\Spool\Printers\*.*
+
+echo Reiniciando o servico de impressao...
+echo.
+
+net start spooler
+
+echo Limpeza da fila de impressao concluida.
+Pause
+
