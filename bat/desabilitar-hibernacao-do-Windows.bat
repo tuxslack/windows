86a744c3c04120f254cb94d6bde8074658fb27de
tuxslack <33039864+tuxslack@users.noreply.github.com>
2023-08-28 13:49:31 -0300

    Add files via upload

---
+++ bat/desabilitar-hibernacao-do-Windows.bat
@@ -0,0 +1,30 @@
+@echo off
+
+cls
+
+echo
+echo Tornar a hibernação indisponível
+echo ================================
+echo
+
+powercfg -h off
+
+rem powercfg.exe /hibernate off
+
+
+echo
+echo Reinicia o sistema agora... Caso queira reativar função de hibernação, basta abrir novamente o Powershell ou CMD como administrador e entrar com o comando:
+echo 
+echo Disponibilizar a hibernação
+echo ===========================
+echo 
+echo powercfg -h on
+echo
+echo ou
+echo
+echo powercfg.exe /hibernate on
+echo 
+
+
+Pause
+
